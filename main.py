463c680930c3a7ffe46170026d8c777fc6dff330
robotescu <80849623+robotescu@users.noreply.github.com>
2022-02-01 20:00:17 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ lista = []
 def on_button_pressed_a():
     global value
     value+=1
+    basic.show_number(value)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global value
-    lista.append(value)
+    lista.push(value)
     value = 0
+    basic.show_number(value)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_button_pressed_ab():
     global lista
+    lista.sort()
     basic.show_string("Cresc")
     for i in range(0,lista.length):
         basic.show_number(lista[i])
--- main.ts
+++ main.ts
@@ -4,15 +4,18 @@ lista = []
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     value += 1
+    basic.showNumber(value)
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
     lista.push(value)
     value = 0
+    basic.showNumber(value)
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     let i: number;
     
+    lista.sort()
     basic.showString("Cresc")
     for (i = 0; i < lista.length; i++) {
         basic.showNumber(lista[i])
